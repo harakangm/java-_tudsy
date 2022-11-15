6c510633ae62cb48fb111bff85a00cf79291a71f
github harakangm <github diehfkszld@gmail.com>
2022-11-15 18:31:53 +0900

    up

--- src/test/ch06/exam01/pakage1/A.java
+++ src/test/ch06/exam01/pakage1/A.java
@@ -1,5 +1,29 @@
 package test.ch06.exam01.pakage1;
 
-public class A {
+// 클래스는 public , default 접근 제한자를 가질 수 있다.
+// 생산자에는 public, default, private 접근제한자를 사용가능
+// 아무것도 안 쓴 상태를 default 이다.
+// public 패키지를 뛰어넘어서 사용가능 / default 패키지를 뛰어넘어서 사용불가
 
+public class A {
+	//1. 접근 제한자를 먹일수 있는곳
+	// 클래스, 필드 , 생성자, 메소드
+	
+	A a3 = new A("문자열");
+	  
+	public A(boolean b) {
+		
+	}
+	 
+	A(int b) {
+		
+	}
+	 
+	// 같은 클래스 안에서만 호출 가능
+	private A(String b) {
+		
+	}
+	
+
+	
 }
--- src/test/ch06/hyundai/Car.java
+++ src/test/ch06/hyundai/Car.java
@@ -1,9 +1,15 @@
 package test.ch06.hyundai;
 
 import test.ch06.hankook.SnowTire;
+import test.ch6.kumho.AllSeasonTire;
 
 public class Car {
 	//다른 패키지에 있는 클래스를 사용 하기위해서는 반드시 임포트를 해야함
 	SnowTire a = new SnowTire();
-	Test b 
+	AllSeasonTire allt = new AllSeasonTire();
+
+	
+	//import를 안하고 클래스 사용법
+	
+	test.ch06.hankook.Tire tire1 = new test.ch06.hankook.Tire();
 }
